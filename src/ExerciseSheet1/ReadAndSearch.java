9f8d0844d10f155f8c315b4c75bdc3956b88b91f
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-09-17 17:50:37 +0200

    c)
    Escreva um programa para ler N inteiros, ler uma chave a e verificar se consta no conjunto de N
    inteiros lidos (Use recursos de java para entrada de dados, pesquisar, etc)

--- src/ExerciseSheet1/ReadAndSearch.java
+++ src/ExerciseSheet1/ReadAndSearch.java
@@ -1,5 +1,44 @@
 package ExerciseSheet1;
 
-public class ReadAndSearch {
+import java.util.Scanner;
 
+public class ReadAndSearch {
+	private static Scanner reader = new Scanner(System.in);
+	static boolean status = false;
+
+	public static void main(String[] args) {
+
+		ReadAndprintIntegerNumbers();
+	}
+
+	public static void ReadAndprintIntegerNumbers() {
+
+		System.out.print("How many numbers you want to enter?");
+		int numbers = reader.nextInt();
+		int[] listOfNumers = new int[numbers];
+
+		for (int i = 0; i < numbers; i++) {
+			int next = i + 1;
+			System.out.println("what's number " + next + " ?");
+			listOfNumers[i] = reader.nextInt();
+
+		}
+
+		System.out.println("what is number you want to find?");
+		int numbersfind = reader.nextInt();
+		for (int i = 0; i < numbers; i++) {
+			if (listOfNumers[i] == numbersfind) {
+
+				System.out.print("The number you searchead exists and is in position " + i);
+				status = true;
+			}
+
+		}
+
+		if (status == false) {
+			System.out.println("The number you searchead does not nexist ");
+
+		}
+
+	}
 }
--- src/ExerciseSheet1/ReadNIntegerSortAndPrint.java
+++ src/ExerciseSheet1/ReadNIntegerSortAndPrint.java
@@ -4,31 +4,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadNIntegerSortAndPrint {
-	
 
 	private static Scanner reader = new Scanner(System.in);
+
 	public static void main(String[] args) {
 
 		ReadAndprintIntegerNumbers();
 	}
 
 	public static void ReadAndprintIntegerNumbers() {
-		
-		System.out.print("How many numbers you want to print?");
+
+		System.out.print("How many numbers you want to order?");
 		int numbers = reader.nextInt();
 		int[] listOfNumers = new int[numbers];
-		
-		for (int i =0 ; i < numbers; i++) {
-			int next = i+1;
-			System.out.println("what's number " +next + " ?");
-			listOfNumers[i] =  reader.nextInt();
-			Arrays.sort(listOfNumers);
-			
-		}
-		
-			System.out.print(" "+ Arrays.toString(listOfNumers));
-		
-	}
 
+		for (int i = 0; i < numbers; i++) {
+			int next = i + 1;
+			System.out.println("what's number " + next + " ?");
+			listOfNumers[i] = reader.nextInt();
+			Arrays.sort(listOfNumers);
+
+		}
+
+		System.out.print(" " + Arrays.toString(listOfNumers));
+
+	}
 
 }
