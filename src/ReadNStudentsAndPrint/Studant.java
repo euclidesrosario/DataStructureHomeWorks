35ba74c3023ba9888788e99fefdfee0fb67cb4f9
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-09-18 13:28:13 +0200

    Create Studant.java

---
+++ src/ReadNStudentsAndPrint/Studant.java
@@ -0,0 +1,69 @@
+package ReadNStudentsAndPrint;
+
+public class Studant {
+	private String nome;
+	private int codigo;
+	private int idade;
+	public Studant() {}
+	
+	
+	public Studant(String nome, int codigo, int idade) {
+		super();
+		this.nome = nome;
+		this.codigo = codigo;
+		this.idade = idade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Studant other = (Studant) obj;
+		if (codigo != other.codigo)
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Studant [nome=" + nome + ", codigo=" + codigo + ", idade=" + idade + "]";
+	}
+	
+
+}
